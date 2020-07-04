ec284258043b0d411d7d4ffd6527f855ab5a9d62
shuntakeuch1 <takeuchishun89@gmail.com>
2020-07-04 13:50:59 +0900

    add mysql db access

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,8 @@ repositories {
 dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+    val mySQLVersion = "5.1.34"
+    implementation("mysql:mysql-connector-java:$mySQLVersion")
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
